5c475911d5ea50e53f0f0a4e4dc869260fde2c31
bhavi1902 <70758812+bhavi1902@users.noreply.github.com>
2020-09-26 01:16:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+#print("hemlo guys")
+pi=3.141592653589793238
+radius= float(input("Enter the radius of a circle:"))
+area=pi*radius*radius
+print("The radius of the circle with radius {} is:".format(radius),area)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
